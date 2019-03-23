b07309a6da5cf23e38679bd3e2aec7f94cd568b4
Foarvad <foarvad@gmail.com>
2019-03-23 11:49:32 +0300

    Restruction, change amount

---
+++ src/components/Market/index.js
@@ -0,0 +1,37 @@
+// src/components/Market/index.js
+
+import React, { Component } from 'react';
+import './styles.css';
+
+class Market extends Component {
+    render() {
+        return (
+            <div>
+                <table id="market-table">
+                    <tbody>
+                        <tr>
+                            <td className="market-td"><img className="crypto-logo" src={require("./img/BTC.png")} alt="BTC-logo" />BTC</td>
+                            <td className="market-td">
+                                <div className="crypto-price">$3581</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="market-td"><img className="crypto-logo" src={require("./img/ETH.png")} alt="ETH-logo" />ETH</td>
+                            <td className="market-td">
+                                <div className="crypto-price">$149</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="market-td" style={{ borderBottom: '0' }}><img className="crypto-logo" src={require("./img/EOS.png")} alt="EOS-logo" />EOS</td>
+                            <td className="market-td" style={{ borderBottom: '0' }}>
+                                <div className="crypto-price">$0.32</div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default Market
